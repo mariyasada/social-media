8b0f148286ea17a5aa7eddbaf7f19990f02f73d3
mariyasada <mariyasada2406@gmail.com>
2022-05-31 21:32:30 +0530

    Update firebaseconfige.js

--- src/firebaseconfige.js
+++ src/firebaseconfige.js
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: proccess.env.REACT_APP_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: "insta-connect-15e55",
   storageBucket: "insta-connect-15e55.appspot.com",
